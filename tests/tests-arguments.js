92df3240d751fd4b40de1075ff272e44cf43f34e
Pawel Rafalko <dev.rafalko@gmail.com>
2017-08-27 01:39:23 +0200

    Tests descriptions corrected.

--- tests/tests-arguments.js
+++ tests/tests-arguments.js
@@ -3,8 +3,8 @@ const path = require('path');
 const type = require(path.resolve('./index.js'));
 const scenario = require('./scenarios-arguments.js');
 
-describe('The module function type() is called (regardless the callback is defined) with the arguments',function(){
-  describe('when callback is defined, it should throw TypeError with message',function(){
+describe('When the module function type() is called',function(){
+  describe('with callback defined and the arguments',function(){
     for(let i in scenario){
       it(`type(${scenario[i].oActual},${scenario[i].oExpected}), it should throw TypeError with message "${scenario[i].message}"`,function(){
         let binded = type.bind(this,scenario[i].actual,scenario[i].expected,(o)=>{});
@@ -12,7 +12,7 @@ describe('The module function type() is called (regardless the callback is defin
       });
     }
   });
-  describe('when callback is not defined, it should throw TypeError with message',function(){
+  describe('without callback defined and the arguments',function(){
     for(let i in scenario){
       it(`type(${scenario[i].oActual},${scenario[i].oExpected}), it should throw TypeError with message "${scenario[i].message}"`,function(){
         let binded = type.bind(this,scenario[i].actual,scenario[i].expected,(o)=>{});
